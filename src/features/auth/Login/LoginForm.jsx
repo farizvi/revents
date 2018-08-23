998b4b557e7d84912e15580986fbaf0cdafba43a
farizvi <fahad.rizvi1@outlook.com>
2018-08-23 16:45:24 +1000

    End of Section 15

--- src/features/auth/Login/LoginForm.jsx
+++ src/features/auth/Login/LoginForm.jsx
@@ -1,36 +1,31 @@
 import React from 'react';
 import { connect } from 'react-redux'
-import { Form, Segment, Button } from 'semantic-ui-react';
+import { Form, Segment, Button, Label, Divider } from 'semantic-ui-react';
 import { Field, reduxForm } from 'redux-form';
 import TextInput from '../../../app/common/form/TextInput';
-import { login } from '../authActions';
+import { login, socialLogin } from '../authActions';
+import SocialLogin from '../SocialLogin/SocialLogin'
 
 const actions = {
-  login
+  login,
+  socialLogin
 }
 
-const LoginForm = ({login, handleSubmit}) => {
-  return (
-    <Form error size="large" onSubmit={handleSubmit(login)}>
+const LoginForm = ({login, handleSubmit, error, socialLogin}) => {
+  return <Form size="large" onSubmit={handleSubmit(login)}>
       <Segment>
-        <Field
-          name="email"
-          component={TextInput}
-          type="text"
-          placeholder="Email Address"
-        />
-        <Field
-          name="password"
-          component={TextInput}
-          type="password"
-          placeholder="password"
-        />
+        <Field name="email" component={TextInput} type="text" placeholder="Email Address" />
+        <Field name="password" component={TextInput} type="password" placeholder="password" />
+        {error && <Label basic color="red">
+            {error}
+          </Label>}
         <Button fluid size="large" color="teal">
           Login
         </Button>
+        <Divider horizontal>Or</Divider>
+        <SocialLogin socialLogin={socialLogin} />
       </Segment>
-    </Form>
-  );
+    </Form>;
 };
 
 export default connect(null, actions)(reduxForm({form: 'loginForm'})(LoginForm));
--- src/features/nav/NavBar/NavBar.jsx
+++ src/features/nav/NavBar/NavBar.jsx
@@ -3,17 +3,17 @@ import { Menu, Container, Button } from 'semantic-ui-react'
 import { NavLink, Link, withRouter } from 'react-router-dom'
 import SignedOutMenu from '../Menus/SignedOutMenu';
 import SignedInMenu from '../Menus/SignedInMenu';
-import { connect } from 'react-redux'
+import { connect } from 'react-redux';
+import { withFirebase } from 'react-redux-firebase';
 import { openModal } from '../../modals/modalActions'
-import { logout } from '../../auth/authActions';
 
 const actions = {
-    openModal,
-    logout
+    openModal
 }
 
 const mapState = (state) => ({
-    auth: state.auth
+    auth: state.firebase.auth,
+    profile: state.firebase.profile
 })
 
 class NavBar extends Component {
@@ -27,13 +27,13 @@ class NavBar extends Component {
   }
 
   handleSignOut = () => {
-    this.props.logout();
+    this.props.firebase.logout();
     this.props.history.push('/') ;  
   }
 
   render() {
-    const {auth} = this.props;
-    const authenticated = auth.authenticated;
+    const {auth, profile} = this.props;
+    const authenticated = auth.isLoaded && !auth.isEmpty;
     return (        
         <Menu inverted fixed="top">
             <Container>
@@ -51,7 +51,7 @@ class NavBar extends Component {
                         <Button as={Link} to='/createEvent' floated="right" positive inverted content="Create Event" />
                     </Menu.Item>}
                     
-                {authenticated ? <SignedInMenu currentUser={auth.currentUser} signOut={this.handleSignOut} /> 
+                {authenticated ? <SignedInMenu profile={profile} signOut={this.handleSignOut} /> 
                                 : <SignedOutMenu signIn={this.handleSignIn} register={this.handleRegister} />}                                
             </Container>
         </Menu>
@@ -59,4 +59,4 @@ class NavBar extends Component {
   }
 }
 
-export default withRouter(connect(mapState, actions)(NavBar));
+export default withRouter(withFirebase(connect(mapState, actions)(NavBar)));
